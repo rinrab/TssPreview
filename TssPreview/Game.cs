090a875d4a41d58809be788298b13de424a366eb
rinrab <tima@chemodax.net>
2023-01-20 16:03:14 +0300

    Format code

--- TssPreview/Game.cs
+++ TssPreview/Game.cs
@@ -18,6 +18,7 @@ namespace TssPreview
 
         [DataMember(Name = "width")]
         public int Width { get; set; }
+
         [DataMember(Name = "height")]
         public int Height { get; set; }
 
